146161d31b92ffd12d71cd0db7a86bd1ff605a6e
yaishenka <gagarinovdaniil@gmail.com>
2017-12-01 11:28:41 +0300

    AVL1.1

--- projectAVL.cpp
+++ projectAVL.cpp
@@ -7,7 +7,8 @@
 
 using namespace std;
 
-struct Node {
+struct Node
+{
     Node(int key) : key(key) {}
     Node* left;
     Node* right;
@@ -15,13 +16,15 @@ struct Node {
     int height = 1;
 }
 
-int height(Node* node) {
+int height(Node* node)
+{
   if (node == nullptr)
   return 0;
   else return (node->height);
 }
 
-int diff(Node* node) {
+int diff(Node* node)
+{
   if (node == nullptr) return 0;
   else return (height(node->left) - height(node->right));
 }
@@ -31,7 +34,8 @@ void updateHeight(Node * node)
   node->height = 1 + max(height(node->left), height(node->right));
 }
 
-Node * rotateLeft(Node * node) {
+Node * rotateLeft(Node * node)
+{
     Node * b = node->right;
     node->right = b->left;
     b->left = node;
@@ -40,7 +44,8 @@ Node * rotateLeft(Node * node) {
     return b;
 }
 
-Node * rotateRight(Node * node) {
+Node * rotateRight(Node * node)
+{
     Node * b = node->left;
     node->left = b->right;
     b->right = node;
@@ -49,15 +54,20 @@ Node * rotateRight(Node * node) {
     return b;
 }
 
-Node * balance(Node * node) {
+Node * balance(Node * node)
+{
     updateHeight(node);
-    if(diff(node) == 2) {
-        if(diff(node->right<0) {
+    if(diff(node) == 2)
+    {
+        if(diff(node->right<0)
+        {
             rotateRight(node->right);
         }
         return rotateLeft(node);
-    } else if(diff(node) ==-2) {
-        if(diff(node->left>0) {
+    } else if(diff(node) ==-2)
+    {
+        if(diff(node->left>0)
+        {
             rotateLeft(node->left);
         }
         return rotateRight(node);
@@ -65,50 +75,64 @@ Node * balance(Node * node) {
     return node;
 }
 
-Node * insert(Node * node, int key) {
-    if(node == nullptr) {
+Node * insert(Node * node, int key)
+{
+    if(node == nullptr)
+    {
         return new Node(key);
     }
-    if(node->key > key) {
+    if(node->key > key)
+    {
         node->left = insert(node->left, key);
-    } else {
+    } else
+    {
         node->right = insert(node->right, key);
     }
 
     return balance(node);
 }
 
-Node * findMin(Node * node) {
-    if(node->left == nullptr) {
+Node * findMin(Node * node)
+{
+    if(node->left == nullptr)
+    {
         return node;
-    } else {
+    } else
+    {
         return findParent(node->left);
     }
 }
 
-Node * deleteMin(Node * node) {
-    if(node->left == nullptr) {
+Node * deleteMin(Node * node)
+{
+    if(node->left == nullptr)
+    {
         return node->right;
     }
     node->left = deleteMin(node->left);
     reutrn balance(node);
 }
 
-Node * delete(Node * node, int key) {
-    if(node->key = key) {
-        Node * min = findMin(node->right);
+Node * delete(Node * node, int key)
+{
+    if(node->key = key)
+    {
+        Node * min = findMin(node -> right);
         node->key = min->key;
-        node->right = deleteMin(node->right);
+        node->right = deleteMin(node -> right);
         return balance(node);
-    } else if(node->key > key) {
-      delete(node->left, key);
-    } else {
-      delete(node->right, key);
+    } else if(node -> key > key)
+    {
+      delete(node -> left, key);
+    } else
+    {
+      delete(node -> right, key);
     }
-    return balance(node);
+    return balance (node);
 }
 
 
-int main(){
+int main ()
+{
     return 0;
 }
