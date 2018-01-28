69bd96f7544358f01c65f684790e358867000a3e
yaishenka <gagarinovdaniil@gmail.com>
2018-01-28 16:15:02 +0300

    sharpdich

---
+++ FirstStepSharp01/FirstStepSharp01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstStepSharp01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FirstStepSharp01/FirstStepSharp02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstStepSharp02
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("ВвЕдИ СтРоКу ДрУжоК))");
+            string question = Console.ReadLine();
+            System.Text.StringBuilder sb = new System.Text.StringBuilder(question);
+            for (int j = 0; j < sb.Length; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    sb[j] = System.Char.ToUpper(sb[j]);
+                }
+            }
+            string corrected = sb.ToString();
+            System.Console.WriteLine(corrected);
+        }
+    }
+}
