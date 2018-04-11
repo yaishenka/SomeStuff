e564ef0c9c9f0b3751d1cdf9cb73b04a21c4a725
yaishenka <gagarinovdaniil@gmail.com>
2018-04-11 14:22:52 +0300

    task5notsolv

--- SomeDAFEstudy/алгоритмы/2 семестр/ass#2/task5.cpp
+++ SomeDAFEstudy/алгоритмы/2 семестр/ass#2/task5.cpp
@@ -37,7 +37,7 @@ public:
         graph_d += manh_d;
     }
     uint64_t hashize() const {
-        uint64_t a;
+        uint64_t a=0;
         for (int i(0); i < size; ++i) {
             for (int j(0); j < size; ++j) {
                 a |= (field[i][j] & 0xF) << (i + j);
@@ -129,10 +129,10 @@ public:
             pw.push_back(R());
         }
         if (zero_in.first > 0) {
-            pw.push_back(U());
+            pw.push_back(D());
         }
         if (zero_in.first < size-1) {
-            pw.push_back(D());
+            pw.push_back(U());
         }
         return pw;
     }
@@ -144,8 +144,8 @@ public:
         tmp.second = zero_in.second - parent->zero_in.second;
         if (tmp.second > 0) return 'L';
         if (tmp.second < 0) return 'R';
-        if (tmp.first > 0) return 'D';
-        if (tmp.first < 0) return 'U';
+        if (tmp.first > 0) return 'U';
+        if (tmp.first < 0) return 'D';
         return ' ';
     }
 };
@@ -182,9 +182,13 @@ string AStar(Field& p) {
     while (!pif.empty()) {
         Field current = *pif.begin();
         pif.erase(pif.begin());
+        visited.emplace(current);
+        std::cout << "i pick a new curr: ";
+        std::cout << current << std::endl;
         std::vector<Field> pw = current.PossibleWays();
         for (auto child : pw) {
-            //std::cout << child;
+            std::cout << "i pick a new child: ";
+            std::cout << child << std::endl;
             if (child.manh_d == 0) {
                 string ans;
                 const Field * thiz = &child;
@@ -195,6 +199,7 @@ string AStar(Field& p) {
                 return ans;
             } else {
                 if (visited.find(child) == visited.end() && pif.find(child) == pif.end()) {
+                    std::cout << "i push that child" << std::endl << std::endl;
                     child.graph_d +=  current.graph_d + 1;
                     pif.emplace(child);
                 }
